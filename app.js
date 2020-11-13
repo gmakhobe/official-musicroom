320adb06988da019a6136235dc63cadb53f08f0e
Smashkat12 <smashkat12@gmail.com>
2020-11-13 16:20:49 +0200

    added create playlist socket

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 const morgan = require("morgan");
 const chalk = require("chalk");
@@ -10,10 +9,11 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const authRouter = require("./router/authRouter");
 const userRouter = require("./router/userRouter");
-const playlistRouter = require("./router/playlistRouter")
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const Playlist = require("./model/PlaylistModel");
+const axios = require("axios");
 
 const port = 5000;
 
@@ -43,16 +43,16 @@ mongoose.connection.on("disconnected", () => {
   cl(chalk.bold.redBright("The db has disconnected!"));
 });
 
-// End MongoBD Connection 
+// End MongoBD Connection
 
 // view engine setup
-app.set('views', path.join(__dirname, 'view')); //Fetch views under /view
-app.set('view engine', 'ejs'); // Initiate view engine
+app.set("views", path.join(__dirname, "view")); //Fetch views under /view
+app.set("view engine", "ejs"); // Initiate view engine
 app.use(morgan("dev")); // Enable terminal logging
-app.use(bodyParser.urlencoded({ extended: false, limit: '25mb' })); // Max upload size
+app.use(bodyParser.urlencoded({ extended: false, limit: "25mb" })); // Max upload size
 app.use(bodyParser.json()); // Enable upload of a request with body data
 app.use(cookieParser()); //Enable cookie storage
-app.use(express.static(path.join(__dirname, 'public'))); // Fetch static content in public folder eg css js images etc
+app.use(express.static(path.join(__dirname, "public"))); // Fetch static content in public folder eg css js images etc
 
 //passport
 require("./services/passport-service")(passport); //pass same instance of passport to be used in config.
@@ -63,17 +63,83 @@ app.use(require("./router/Index"));
 
 app.use("/api/auth", authRouter.router);
 app.use("/api/user", userRouter.router);
-app.use("/api/playlist", playlistRouter.router);
+
 // End Use routers
 
-
 io.of("/api/playlist").on("connection", (socket) => {
-	socket.emit("welcome", "this was just a test")
-})
+  socket.emit("welcome", "this was just a test");
 
+  socket.on("create playlist", (playlistInfo) => {
+    Playlist.findOne({ name: playlistInfo.title })
+      .then((ExistingPlaylist) => {
+        if (ExistingPlaylist) {
+          socket.emit(
+            "create playlist error",
+            "Playlist with a name already exist!"
+          );
+        } else {
+          const url = `https://api.deezer.com/user/${playlistInfo._deezerId}/playlists?title=${playlistInfo.title}&access_token=${playlistInfo.deezerToken}`;
 
+          axios
+            .post(url)
+            .then((result) => {
+              const parameters = {
+                _deezerPId: result.data.id,
+                name: playlistInfo.title,
+                type: playlistInfo.type,
+              };
+
+              const Newplaylist = new Playlist(parameters);
+
+              Newplaylist.save((error, playlist) => {
+                if (error) {
+                  socket.emit(
+                    "server error",
+                    "there was a problem saving the playlist in the db"
+                  );
+                }
+                Playlist.findByIdAndUpdate(
+                  { _id: playlist._id },
+                  {
+                    $push: {
+                      users: { id: req.user._id, role: "RW", creator: true },
+                    },
+                  },
+                  { new: true },
+                  (err, newPlaylist) => {
+                    if (err) {
+                      socket.emit(
+                        "server error",
+                        "there was a problem saving the playlist in the db"
+                      );
+                    }
+
+                    socket.emit("create playlist success", {
+                      message: "playlist successfully created",
+                      playlist: newPlaylist,
+                    });
+                  }
+                );
+              });
+            })
+            .catch((err) => {
+              socket.emit(
+                "server error",
+                "there was a problem saving the playlist in the db"
+              );
+            });
+        }
+      })
+      .catch((err) => {
+        socket.emit(
+          "server error",
+          "there was a problem saving the playlist in the db"
+        );
+      });
+  });
+});
 
 //App should listen to request on port ${port}
 http.listen(port, () => {
-    console.log(`Go to http://localhost:${port} on your browser`)
+  console.log(`Go to http://localhost:${port} on your browser`);
 });
