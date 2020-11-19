7f89af30a130b90c665847e3d9dc5b3b16f094f4
Thabang Chewa <50702994+tchewaa@users.noreply.github.com>
2020-11-19 11:17:42 +0200

    added tags to categorize api

--- router/searchRouter.js
+++ router/searchRouter.js
@@ -5,16 +5,29 @@ const searchHandler = require("../controller/SearchController");
 exports.router = (() => {
     const searchRouter = express.Router();
 
-    /**
+/**
  * @swagger
  * /api/search:
  *  get:
- *      description: Searches the track by name
+ *      tags:
+ *        - Search
+ *      description: Track search
+ *      parameters:
+ *          - in: query
+ *            name: type
+ *            schema:
+ *              type: string
+ *            description: Search by providing the name of the artist or name of the song
+ *          - in: query
+ *            name: q
+ *            schema:
+ *              type: string
+ *            description: Provide the search type, which is Track
  *      responses:
  *          200:
- *              description: display all tracks by name/artist name
+ *              description: Display all tracks by name/artist name
  *          401:
- *              description: requires authentication
+ *              description: Requires Authentication
  */
     
     searchRouter.get("/", searchHandler.search);
