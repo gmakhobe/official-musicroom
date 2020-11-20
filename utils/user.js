1c7221bb494d72865ca5691aff54f4f6c6000838
Smashkat12 <smashkat12@gmail.com>
2020-11-20 10:56:12 +0200

    fixed add track broadcast bug

--- utils/user.js
+++ utils/user.js
@@ -1,13 +1,11 @@
-const users = require("deezer-web-api/src/users");
-
-const user = [];
+const users = [];
 
 //join user to room
 
 function userJoin(socketId, name, roomId){
 	const user = { socketId, name, roomId };
 
-	user.push(user);
+	users.push(user);
 
 	return user;
 }
